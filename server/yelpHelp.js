c4d072e7feec2d85cb7a6aea601666bc07027ec1
Justin Seiter <justinseiter@gmail.com>
2016-02-15 04:24:55 -0600

    Update yelp env var name.

--- server/yelpHelp.js
+++ server/yelpHelp.js
@@ -7,7 +7,7 @@ var client = Yelp.createClient({
     "consumer_key": process.env.YELP_CONSUMER_KEY,
     "consumer_secret": process.env.YELP_CONSUMER_SECRET,
     "token": process.env.YELP_TOKEN,
-    "token_secret": process.env.YELP_SECRET
+    "token_secret": process.env.YELP_TOKEN_SECRET
   }
 });
 
