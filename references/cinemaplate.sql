6274f4a5ccf5855c7712b5066a1c8a42d7a5cc9f
Carly Levy <carlylevy@gmail.com>
2016-02-16 15:27:06 -0600

    updating data model. replacing genre cuisines with suggested pairings between movies and restaurants

--- references/cinemaplate.sql
+++ references/cinemaplate.sql
@@ -1,12 +1,13 @@
-CREATE TABLE IF NOT EXISTS "Genre" (
+CREATE TABLE IF NOT EXISTS "genres" (
   "genre_id" SERIAL NOT NULL,
   "genre_name" varchar(100) NOT NULL,
-  CONSTRAINT Genre_pk PRIMARY KEY ("genre_id")
+  "genre_moviedb_id" INTEGER NOT NULL,
+  CONSTRAINT genres_pk PRIMARY KEY ("genre_id")
 );
 
 
 
-CREATE TABLE IF NOT EXISTS "Movie" (
+CREATE TABLE IF NOT EXISTS "movies" (
   "movie_id" SERIAL NOT NULL,
   "movie_title" varchar(255) NOT NULL,
   "movie_summary" TEXT NOT NULL,
@@ -14,12 +15,12 @@ CREATE TABLE IF NOT EXISTS "Movie" (
   "movie_release_year" integer(4) NOT NULL,
   "movie_director" varchar(255) NOT NULL,
   "movie_actors" TEXT NOT NULL,
-  CONSTRAINT Movie_pk PRIMARY KEY ("movie_id")
+  CONSTRAINT movies_pk PRIMARY KEY ("movie_id")
 );
 
 
 
-CREATE TABLE IF NOT EXISTS "Restaurant" (
+CREATE TABLE IF NOT EXISTS "restaurants" (
   "restaurant_id" SERIAL NOT NULL,
   "restaurant_name" varchar(255) NOT NULL,
   "restaurant_description" TEXT NOT NULL,
@@ -30,51 +31,49 @@ CREATE TABLE IF NOT EXISTS "Restaurant" (
   "restaurant_image_url" TEXT NOT NULL,
   "restaurant_url" TEXT NOT NULL,
   "restaurant_yelp_rating" DECIMAL NOT NULL,
-  CONSTRAINT Restaurant_pk PRIMARY KEY ("restaurant_id")
+  CONSTRAINT restaurants_pk PRIMARY KEY ("restaurant_id")
 );
 
 
 
-CREATE TABLE IF NOT EXISTS "RestaurantCuisine" (
-  "restaurant_cuisine_id" SERIAL NOT NULL,
-  "fk_restaurant_id" integer NOT NULL REFERENCES Restaurant(restaurant_id) ON DELETE CASCADE,
-  "fk_cuisine_id" integer NOT NULL REFERENCES Cuisine(cuisine_id) ON DELETE RESTRICT,
-  CONSTRAINT RestaurantCuisine_pk PRIMARY KEY ("restaurant_cuisine_id")
-);
-
-
-
-CREATE TABLE IF NOT EXISTS "StreamingUrl" (
-  "url_id" SERIAL NOT NULL,
-  "url" TEXT NOT NULL,
-  "fk_movie_id" integer NOT NULL,
-  CONSTRAINT StreamingUrl_pk PRIMARY KEY ("url_id"),
-  CONSTRAINT StreamingUrl_fk0 FOREIGN KEY (fk_movie_id) REFERENCES Movie(movie_id)
-);
-
-
-
-CREATE TABLE IF NOT EXISTS "Cuisine" (
+CREATE TABLE IF NOT EXISTS "cuisines" (
   "cuisine_id" SERIAL NOT NULL,
   "cuisine_name" varchar(255) NOT NULL,
   "cuisine_yelp_category_id" varchar(255) NOT NULL UNIQUE,
-  CONSTRAINT Cuisine_pk PRIMARY KEY ("cuisine_id")
+  CONSTRAINT cuisines_pk PRIMARY KEY ("cuisine_id")
 );
 
 
 
-CREATE TABLE IF NOT EXISTS "MovieGenre" (
-  "fk_movie_id" integer NOT NULL REFERENCES Movie(movie_id) ON DELETE CASCADE,
-  "fk_genre_id" integer NOT NULL REFERENCES Genre(genre_id) ON DELETE RESTRICT,
-  CONSTRAINT MovieGenre_pk PRIMARY KEY (fk_movie_id, fk_genre_id)
+CREATE TABLE IF NOT EXISTS "restaurantCuisines" (
+  "restaurant_cuisine_id" SERIAL NOT NULL,
+  "fk_restaurant_id" integer NOT NULL REFERENCES restaurants(restaurant_id) ON DELETE CASCADE,
+  "fk_cuisine_id" integer NOT NULL REFERENCES cuisines(cuisine_id) ON DELETE RESTRICT,
+  CONSTRAINT restaurantCuisines_pk PRIMARY KEY ("restaurant_cuisine_id")
 );
 
 
 
-CREATE TABLE IF NOT EXISTS "CuisineGenre" (
-  "fk_cuisine_id" integer NOT NULL,
-  "fk_genre_id" integer NOT NULL,
-  CONSTRAINT CuisineGenre_pk PRIMARY KEY (fk_cuisine_id, fk_genre_id),
-  CONSTRAINT CuisineGenre_fk0 FOREIGN KEY (fk_cuisine_id) REFERENCES Cuisine(cuisine_id),
-  CONSTRAINT CuisineGenre_fk1 FOREIGN KEY (fk_genre_id) REFERENCES Genre(genre_id)
+CREATE TABLE IF NOT EXISTS "streamingUrls" (
+  "url_id" SERIAL NOT NULL,
+  "url" TEXT NOT NULL,
+  "fk_movie_id" integer NOT NULL REFERENCES movies(movie_id),
+  CONSTRAINT streamingUrls_pk PRIMARY KEY ("url_id"),
+);
+
+
+
+CREATE TABLE IF NOT EXISTS "movieGenres" (
+  "fk_movie_id" integer NOT NULL REFERENCES movies(movie_id) ON DELETE CASCADE,
+  "fk_genre_id" integer NOT NULL REFERENCES genres(genre_id) ON DELETE RESTRICT,
+  CONSTRAINT movieGenres_pk PRIMARY KEY (fk_movie_id, fk_genre_id)
+);
+
+
+
+CREATE TABLE IF NOT EXISTS "pairings" (
+  "pairing_id" SERIAL NOT NULL,
+  "fk_movie_id" integer NOT NULL REFERENCES movies(movie_id),
+  "fk_restaurant_id" integer NOT NULL REFERENCES restaurants(restaurant_id),
+  CONSTRAINT pairings_pk PRIMARY KEY ("pairing_id")
 );
