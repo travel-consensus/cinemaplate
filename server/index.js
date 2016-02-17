9c0240ea415e2d3f988e8bee2e8d630701a7299a
austinjkovach <kovach.a.j@gmail.com>
2016-02-17 17:46:20 -0600

    Styles loading correctly

--- server/index.js
+++ server/index.js
@@ -86,9 +86,12 @@ yelp.getFoodByZip(78701)
 routes.get('/app-bundle.js',
   browserify('./client/app.js'));
 
+routes.get('/css/app-bundle.css', sass.serve('./client/public/scss/app.scss'));
+
 //
 // Match endpoint to match movie genres with cuisines
 //
+
 routes.get('/api/match/:zip', function(req, res) {
   var zip = req.params.zip;
 
@@ -151,7 +154,6 @@ if (process.env.NODE_ENV !== 'test') {
   app.listen(port);
   console.log("Listening on port", port);
 }
-routes.get('/css/app-bundle.css', sass.serve('./client/public/scss/app.scss'));
 else {
   // We're in test mode; make this file importable instead.
   module.exports = routes;
