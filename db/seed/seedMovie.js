6aa7af8dd5c2d3355fb3b910e5ea341736e2e85c
Justin Seiter <justinseiter@gmail.com>
2016-02-22 21:58:25 -0600

    moving seed files and helpers

---
+++ db/seed/seedMovie.js
@@ -0,0 +1,90 @@
+var reddit = require('./helpers/redditHelp');
+var movie = require('./helpers/movieHelp');
+var pg = require('pg');
+
+//
+// Get PG config'd
+//
+var pgConString = '';
+if (process.env.NODE_ENV !== 'production') {
+  // If trying to connect to DB remotely (ie, dev environment)
+  // we need to add the ssl flag.
+  pgConString = process.env.DATABASE_URL + '?ssl=true';
+} else {
+  pgConString = process.env.DATABASE_URL;
+}
+var pgClient = new pg.Client(pgConString);
+
+//
+// START Movie insert
+//
+reddit.getMovies()
+  .then(function(res){
+    return movie.getMovieDB(res)
+  })
+  .then(function(movieData){
+      console.log('I am the response, do with me as you will',movieData)
+      console.log("Total Movies Returned: ", movieData)
+
+      var k;
+      for (k=0;k<movieData.length;k++){
+        (function(){
+          var movieTitle = movieData[k].title
+          var movieSummary = movieData[k].summary
+          var movieUrl = movieData[k].url
+          var movieImageUrl = movieData[k].img
+          var movieRating = movieData[k].rating
+          var movieReleaseDate = movieData[k].releaseDate
+          var movieGenresArray = movieData[k].genreArray
+          var movieGenres = ''
+          // console.log(movieGenresArray.length)
+          if (movieGenresArray.length>0) {
+            for (var g=0;g<movieGenresArray.length;g++){
+              (function(){
+
+              })(g)
+              var thisGenreID = movieData[k].genreArray[g]
+              // console.log("GENRE ID: ", thisGenreID)
+              var sqlGetMovieGenre = 'SELECT genre_name FROM "genres" WHERE genre_moviedb_id=' + thisGenreID
+
+              pgClient.query(sqlGetMovieGenre, function(err, result){
+                if (err){
+                  return console.log("Error getting movie genre", err);
+                }
+                else {
+                  movieGenres += result.rows[0].genre_name
+                  // console.log("INITIAL: ", movieGenres)
+                }
+
+                var resultGenres = movieGenres.slice(0,-2)
+
+              runInsertMovieQuery()
+              })
+              
+            
+            }
+          }
+
+          var runInsertMovieQuery = function(){
+            
+            var sqlInsertMovie = 'INSERT INTO "movies" (movie_title, movie_summary, movie_url, movie_image_url, movie_rating, movie_release_date, movie_genres) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING movie_id'
+            
+            pgClient.query(sqlInsertMovie, [movieTitle, movieSummary, movieUrl, movieImageUrl, movieRating, movieReleaseDate, movieGenres], function (err, result){
+                if (err){
+                  return console.log('error inserting movie', err);
+                }
+                else {
+
+                  console.log("Adding movie >>>", movieTitle)
+                  var newMovieID = result.rows[0].movie_id
+                   newMovieID
+                }
+                  console.log("NEW MOVIE ID: ", newMovieID)
+              })
+          }
+          // });
+        })(k);
+      }
+      console.log(movieData)
+      return;
+  })
---
+++ db/seed/seedRestaurant.js
@@ -0,0 +1,79 @@
+var pg = require('pg');
+var yelp = require('./helpers/yelpHelp');
+
+//
+// Get PG config'd
+//
+var pgConString = '';
+if (process.env.NODE_ENV !== 'production') {
+  // If trying to connect to DB remotely (ie, dev environment)
+  // we need to add the ssl flag.
+  pgConString = process.env.DATABASE_URL + '?ssl=true';
+} else {
+  pgConString = process.env.DATABASE_URL;
+}
+var pgClient = new pg.Client(pgConString);
+
+//
+// START Restaurant insert
+//
+yelp.getFoodByZip(78749)
+.then(function(res){
+    // console.log('i am the res', res); 
+    return res
+})
+.then(function(data){
+    //loop through each restaurant and get restaurant details
+    console.log("Total Restaurants Returned: ", data.length)
+    console.log("Restaurant Object", data[0].location.city)
+    var i;
+    for (i =0;i<data.length;i++){
+      (function(){
+
+        var restName = data[i].name
+        var restDescription = data[i].snippet_text
+        var restPhone = data[i].display_phone
+        var restStreetAddress = data[i].location.display_address[0]
+        var restCity = data[i].location.city
+        var restState = data[i].location.state_code
+        var restZipCode = data[i].location.postal_code
+        var restImageUrl = data[i].image_url
+        var restEat24Url = data[i].eat24_url
+        var restYelpRating = data[i].rating
+        var restYelpId = data[i].id
+        var restCuisinesLength = data[i].categories.length
+        var restCuisines = ''
+        // var newRestaurantID
+
+        // push categories into temp array
+        for (var j=0;j<restCuisinesLength;j++){
+          restCuisines += (data[i].categories[j][0]) + ', '
+        }
+
+        console.log(i+1, ">>>", data[i].name)
+        restCuisines = restCuisines.slice(0,-2)
+
+        pgClient = new pg.Client(pgConString);
+        pgClient.connect(function(err){
+          if (err){
+            return console.log('could not connect to postgres', err);
+          }
+          var sqlInsertRestaurants = 'INSERT INTO "restaurants" (restaurant_name,restaurant_description,restaurant_phone, restaurant_street_address, restaurant_city, restaurant_state, restaurant_zip, restaurant_image_url, restaurant_url, restaurant_yelp_rating, restaurant_yelp_id, restaurant_cuisines) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) RETURNING restaurant_id'
+          
+          pgClient.query(sqlInsertRestaurants, [restName, restDescription, restPhone, restStreetAddress, restCity, restState, restZipCode, restImageUrl, restEat24Url, restYelpRating, restYelpId, restCuisines], function (err, result){
+              if (err){                 
+                return console.log('error inserting restaurant.', err.message);
+              }
+              else {
+                if (result.rows[0].restaurant_id !== undefined){
+                  var newRestaurantID = result.rows[0].restaurant_id
+                  newRestaurantID
+                }
+                console.log("NEW RESTAURANT ID: ", newRestaurantID)
+              }
+            })
+          });
+      })(i);
+    }
+    return;
+  })
