dfecb081d48a9db9265a89bebbf0f515b2d3ab9f
Kamran Varahramyan <kamran@Kamrans-MacBook.local>
2016-02-23 20:03:14 -0600

    initial lock and toggle

--- client/app/match/match.js
+++ client/app/match/match.js
@@ -10,19 +10,40 @@ angular.module('cinePlate.match', [])
     title: 'Kung Fu Panda',
     rating: 4
   }
+  
+  // function that toggles lockRestaurant
+  $scope.restaurantLock = function () {
+    $scope.lockRestaurant = !$scope.lockRestaurant
+  }
+  // function that toggles lockMovie
+  $scope.movieLock = function () {
+    $scope.lockMovie = !$scope.lockMovie
+  }
 
   $scope.searchZip = $routeParams.zip;
   $scope.restaurant = {};
   $scope.movie = {};
-
+  console.log("$scope",$scope);
   $scope.generateMatch = function () {
     $scope.contentLoaded = false;
     $scope.isActive = !$scope.isActive;
     Matches.generateMatch($routeParams.zip)
       .then(function (response) {
         console.log(response)
-        $scope.restaurant = response.restaurant;
-        $scope.movie = response.movie;
+
+        // generate new restaurant unless lockRestaurant is true
+        if(!$scope.lockRestaurant) {
+          $scope.restaurant = response.restaurant;
+          // $scope.lockRestaurant = true;
+        }
+        
+        
+        // generate new movie unless lockMovie is true
+        if(!$scope.lockMovie) {
+          $scope.movie = response.movie;
+          console.log("lockMovie", $scope.lockMovie)
+          // $scope.lockMovie = true;
+        };
         $scope.contentLoaded = true;
         $scope.isActive = !$scope.isActive;
       })
