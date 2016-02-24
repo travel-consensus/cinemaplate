9d9fb3388d068ce1c77a368daef9dbb292cd2041
Jonathan Krone <jkrone@vt.edu>
2016-02-23 19:25:50 -0600

    (feat) improve restaurant model. rebasing

--- db/restaurantModel.js
+++ db/restaurantModel.js
@@ -15,60 +15,61 @@ var pgConConfig = {
 //   // we need to add the ssl flag.
 //   pgConString = process.env.DATABASE_URL + '?ssl=false';
 // } else {
-
 // }
-var pgClient = new pg.Client(pgConConfig);
 
-//
+// var pgClient = new pg.Client(pgConConfig);
+
+
 // START Restaurant insert
-//
-yelp.getFoodByZip(78749)
-.then(function(data){
-    //loop through each restaurant and get restaurant details
-    console.log("Total Restaurants Returned: ", data.length)
+exports = function(zipcode) {
+  yelp.getFoodByZip(zipcode)
+    .then(function(data){
+        //loop through each restaurant and get restaurant details
+        console.log("Total Restaurants Returned: ", data.length)
 
-    pgClient.connect(function(err) {
-      if (err){
-        return console.log('could not connect to postgres', err);
-      }
-      
-      data.map(function(restaurant) {
-        var listing = {
-          name: restaurant.name
-          description: restaurant.snippet_text
-          phone: restaurant.display_phone
-          streetAddress: restaurant.location.display_address[0]
-          city: restaurant.location.city
-          state: restaurant.location.state_code
-          zipCode: restaurant.location.postal_code
-          imageUrl: restaurant.image_url
-          eat24Url: restaurant.eat24_url
-          yelpRating: restaurant.rating
-          yelpId: restaurant.id
-          cuisinesLength: restaurant.categories.length
-          cuisines: ''
-        }
-
-        // push categories into temp array
-        for (var j=0;j<listing.cuisinesLength;j++){
-          listing.cuisines += (restaurant.categories[j][0]) + ', '
-        }
-
-        console.log(i+1, ">>>", data[i].name)
-        restCuisines = restCuisines.slice(0,-2)
-
-        var sqlInsertRestaurants = 'INSERT INTO "restaurants" (restaurant_name,restaurant_description,restaurant_phone, restaurant_street_address, restaurant_city, restaurant_state, restaurant_zip, restaurant_image_url, restaurant_url, restaurant_yelp_rating, restaurant_yelp_id, restaurant_cuisines) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) RETURNING restaurant_id'
-        pgClient.query(sqlInsertRestaurants, [listing.name, listing.description, listing.phone, listing.streetAddress, listing.city, listing.state, listing.zipCode, listing.imageUrl, listing.eat24Url, listing.yelpRating, listing.yelpId, listing.cuisines], function (err, result){
-          if (err){                 
-            return console.log('error inserting restaurant.', err.message);
+        pgClient.connect(pgConConfig, function(err) {
+          if (err){
+            return console.log('could not connect to postgres', err);
           }
-          else {
-            if (result.rows[0].restaurant_id !== undefined){
-              var newRestaurantID = result.rows[0].restaurant_id
+          
+          data.map(function(restaurant) {
+            var listing = {
+              name: restaurant.name,
+              description: restaurant.snippet_text,
+              phone: restaurant.display_phone,
+              streetAddress: restaurant.location.display_address[0],
+              city: restaurant.location.city,
+              state: restaurant.location.state_code,
+              zipCode: restaurant.location.postal_code,
+              imageUrl: restaurant.image_url,
+              eat24Url: restaurant.eat24_url,
+              yelpRating: restaurant.rating,
+              yelpId: restaurant.id,
+              cuisinesLength: restaurant.categories.length,
+              cuisines: ''
             }
-            console.log("NEW RESTAURANT ID: ", newRestaurantID)
-          }
-        }) // pgClient.query
-      }) // restaurant map
-    }) // pgClient connect
-  }
+
+            console.log('restaurant cuisine categories:', restaurant.categories);
+
+            // push categories into temp array
+            for (var j=0;j<listing.cuisinesLength;j++){
+              listing.cuisines += (restaurant.categories[j][0]) + ', '
+            }
+            restCuisines = restCuisines.slice(0,-2)
+
+            var sqlInsertRestaurants = 'INSERT INTO "restaurants" (restaurant_name,restaurant_description,restaurant_phone, restaurant_street_address, restaurant_city, restaurant_state, restaurant_zip, restaurant_image_url, restaurant_url, restaurant_yelp_rating, restaurant_yelp_id, restaurant_cuisines) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) RETURNING restaurant_id'
+            pgClient.query(sqlInsertRestaurants, [listing.name, listing.description, listing.phone, listing.streetAddress, listing.city, listing.state, listing.zipCode, listing.imageUrl, listing.eat24Url, listing.yelpRating, listing.yelpId, listing.cuisines], function (err, result){
+              if (err){                 
+                return console.log('error inserting restaurant.', err.message);
+              }
+              // else {
+              //   if (result.rows[0].restaurant_id !== undefined){
+              //     var newRestaurantID = result.rows[0].restaurant_id
+              //   }
+              //   console.log("NEW RESTAURANT ID: ", newRestaurantID)
+              // }
+            }) // pgClient.query
+          }) // restaurant map
+        }) // pgClient connect
+      }
+}
