30b5e5a54fa8a3357d99a62ab60cbdbed7cdfc79
Chris <nguyen.chriss@gmail.com>
2016-02-25 15:57:57 -0600

    (fix) invalid zip code = 'undefined'

--- client/app/splash/splash.js
+++ client/app/splash/splash.js
@@ -4,6 +4,9 @@ angular.module('cinePlate.splash', ["ngAutocomplete"])
 
 .controller('SplashCtrl', ['$scope', '$http', '$location', function($scope, $http, $location) {
 
+  $scope.PlaceHolder = 'Enter your zip...';
+
+
   $.getJSON('//ip-api.com/json?callback=?', function(data) {
   console.log(JSON.stringify(data, null, 2));
     $scope.zip = data.zip;
@@ -17,7 +20,9 @@ angular.module('cinePlate.splash', ["ngAutocomplete"])
       $location.path('/' + limit_5);
       $scope.$apply()
     } else {
+      $scope.PlaceHolder = 'Invalid zip code!';
     	console.log('no redirect; $scope.zip:', $scope.zip);
+      $scope.zip = '';
     }
   }
 }])
